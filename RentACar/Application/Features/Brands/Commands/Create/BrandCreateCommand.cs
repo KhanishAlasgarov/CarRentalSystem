f36abc322810c9d6f34f98314b03035b4bcde96a
KhanishAlasgarov <khanisha@code.edu.az>
2023-12-26 18:05:41 +0400

    CQRS and Mediator patterns have been implemented, synchronous and asynchronous repositories have been added. Dynamic query functionality has been incorporated into the project.

---
+++ RentACar/Application/Features/Brands/Commands/Create/BrandCreateCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create
+{
+    public class BrandCreateCommand : IRequest<CreatedBrandResponse>
+    {
+        public string? Name { get; set; }
+
+        public class BrandCreateCommandHandler : IRequestHandler<BrandCreateCommand, CreatedBrandResponse>
+        {
+            public async Task<CreatedBrandResponse> Handle(BrandCreateCommand request, CancellationToken cancellationToken)
+            {
+                 
+            }
+        }
+    }
+}
---
+++ corePackages/Core.Packages/Core.Persistance/Repositories/EfRepositoryBase.cs
@@ -0,0 +1,75 @@
+﻿using Core.Persistance.Dynamic;
+using Core.Persistance.Paging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace Core.Persistance.Repositories;
+
+public class EfRepositoryBase<TEntity, TEntityId, TContext> : IAsyncRepository<TEntity, TEntityId>
+    where TEntity : Entity<TEntityId>
+    where TEntityId : unmanaged
+    where TContext : DbContext
+{
+    protected TContext _context { get; }
+
+    public EfRepositoryBase(TContext context)
+    {
+        _context = context;
+    }
+
+    public Task<TEntity> AddAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ICollection<TEntity>> AddRangeAsync(ICollection<TEntity> entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> AnyAsync(Expression<Func<TEntity, bool>>? predicate = null, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> DeleteAsync(TEntity entity, bool permanent = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ICollection<TEntity>> DeleteRangeAsync(ICollection<TEntity> entity, bool permanent = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> expression, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? iclude = null, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Paginate<TEntity>> GetListAsync(Expression<Func<TEntity, bool>>? predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, int index = 0, int size = 10, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Paginate<TEntity>> GetListByDynamicAsync(DynamicQuery dynamic, Expression<Func<TEntity, bool>>? predicate = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, int index = 0, int size = 10, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<TEntity> Query()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ICollection<TEntity>> UpdateRangeAsync(ICollection<TEntity> entity)
+    {
+        throw new NotImplementedException();
+    }
+}
